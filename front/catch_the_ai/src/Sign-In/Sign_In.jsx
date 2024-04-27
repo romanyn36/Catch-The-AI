8d697f8e07ad727e64b4b1c38896060558695c20
romanyn36 <romanyyy88@gmail.com>
2024-04-27 06:00:09 +0300

    style classes renamed to avoid overfitting when page loaded

--- front/catch_the_ai/src/Sign-In/Sign_In.jsx
+++ front/catch_the_ai/src/Sign-In/Sign_In.jsx
@@ -4,6 +4,7 @@ import './Sign_In.css';
 import googleIcon from "./google.png";
 import githubIcon from "./git.png";
 import facebookIcon from "./face.png";
+import { Footer } from "../Footer/Footer";
 
 export class Sign_In extends Component {
     handleGoogleLogin = () => {
@@ -13,38 +14,43 @@ export class Sign_In extends Component {
 
     render() {
         return (
-            <div className="page-container">
-                <div className="signup-container">
-                    <h2 className="UP">Sign In</h2>
-                    <p>Hi,Welcome back</p>
-                    <button className="google-login" onClick={this.handleGoogleLogin}>
-                        <img src={googleIcon} alt="logo" /> Login with Google
-                    </button>
-                    <p>or login with email</p>
-                    <form className="ff">
-                        <div className="form-group">
-                            <label htmlFor="email">Email </label><br />
-                            <input type="text" id="email" name="age" placeholder="Enter your Email" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label><br />
-                            <input type="password" id="password2" name="password2" placeholder="Enter your password" />
-                        </div>
-                        <div className="form-g">
-                            <label> <input type="checkbox" name="remember-me" /> Remember Me  </label>
-                            <a href="/forgot-password" className="forgot-password-link">Forgot Password?</a>
+            <>
+                <div className="page-container">
+                    <div className="signin-container">
+                        <h2 className="UP">Sign In</h2>
+                        <p>Hi,Welcome back</p>
+                        <button className="google-login" onClick={this.handleGoogleLogin}>
+                            <img src={googleIcon} alt="logo" /> Login with Google
+                        </button>
+                        <p>or login with email</p>
+                        <form className="ff">
+                            <div className="form-group">
+                                <label htmlFor="email">Email </label><br />
+                                <input type="text" id="email" name="age" placeholder="Enter your Email" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label><br />
+                                <input type="password" id="password2" name="password2" placeholder="Enter your password" />
+                            </div>
+                            <div className="form-g">
+                                <label> <input type="checkbox" name="remember-me" /> Remember Me  </label>
+                                <a href="/forgot-password" className="forgot-password-link">Forgot Password?</a>
 
-                        </div>
+                            </div>
 
-                        <div className="form-g">
-                            <button type="submit">login</button>
+                            <div className="form-g">
+                                <button type="submit">login</button>
+                            </div>
+                        </form>
+                        <div className="">
+                            <p className="IN">Not Register Yet? <a href="/sign-up" className="INN">Sign Up</a></p>
                         </div>
-                    </form>
-                    <div className="form-group">
-                        <p className="IN">Not Register Yet? <a href="/sign-in" className="INN">Sign In</a></p>
                     </div>
+                    
                 </div>
-            </div>
+                <Footer />
+            </>
+
         );
     }
 }
