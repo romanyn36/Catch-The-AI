f7bf6f97cca654eec37554cd4832f5be6e39fa6b
Romani Nasrat Shawqi <romanyyy88@gmail.com>
2024-05-30 03:24:58 +0300

    bootsratp js added

--- front/catch_the_ai/src/index.js
+++ front/catch_the_ai/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 
 // define base domain url 
