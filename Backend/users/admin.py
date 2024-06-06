d9b65164c4d30bb482fce0497fda972e01d12c4f
Romani Nasrat Shawqi <romanyyy88@gmail.com>
2024-06-06 11:27:48 +0300

    database models moved from ai_detection app to users app

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Subscription,Users,Admin,DataHistory
+
+
+# # # Register your models here.
+admin.site.register(Users)
+admin.site.register(Admin)
+admin.site.register(Subscription)
+admin.site.register(DataHistory)
+
+
+# customizing the admin panel
+admin.site.site_header = "AI Media Detection"
+admin.site.site_title = "AI Media Detection"
