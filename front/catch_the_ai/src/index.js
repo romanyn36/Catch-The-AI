84d8daf302bb9df5d4ae77e0c6d00322b1840554
Romani Nasrat Shawqi <romanyyy88@gmail.com>
2024-06-07 06:08:33 +0300

    domain changed

--- front/catch_the_ai/src/index.js
+++ front/catch_the_ai/src/index.js
@@ -8,9 +8,9 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 
 // define base domain url 
-export var BASE_DOMAIN_URL = 'http://127.0.0.1:8000';
+// export var BASE_DOMAIN_URL = 'http://127.0.0.1:8000';
 
-// export  var BASE_DOMAIN_URL = 'https://romanyn36.pythonanywhere.com';
+export  var BASE_DOMAIN_URL = 'https://romanyn36.pythonanywhere.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
