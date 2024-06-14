3c78e667ffdd3b3bda3021c08f7c46875c3bc949
Romani Nasrat Shawqi <romanyyy88@gmail.com>
2024-06-14 16:23:02 +0300

    new routes

--- front/catch_the_ai/src/App.js
+++ front/catch_the_ai/src/App.js
@@ -24,6 +24,7 @@ import EmailActivation from './Sign_Up/EmailActivation/EmailActivation';
 import ResetPassword from './Sign-In/ResetPassword/ResetPassword';
 import SuccessfullyResetPassword from './Sign-In/ResetPassword/SuccessfullyResetPassword';
 import AudioRecorderUploader from './Navbar/text_detector/AudioRecorderUploader';
+import ContactUs from './Navbar/ContactUs/ContactUs';
 
 
 
@@ -47,21 +48,18 @@ export default class App extends Component {
             <Route path="/Sign-In" element={<Sign_In />} />
             <Route path="/FP" element={<FP />} />
             <Route path="/VP" element={<VP />} />
-
             <Route path="/Sign-Up" element={<SignUp />} />
             <Route path="/footer" element={<Footer />} />
             <Route index path="/UserProfile/:usename" element={<UserProfile />} />
             <Route path="/UpdateProfile" element={<UpdateProfile />} />
             <Route path="/AboutUs" element={<AboutUs />} />
-            <Route path="/AboutUs" element={<AboutUs />} />
+            <Route path="/ContactUs" element={<ContactUs />} />
             <Route path="/UserHistory" element={<UserHistory />} />
             <Route path="/DetectedMedia/:media_id" element={<DetectedMedia />} />
             <Route path="/EmailActivation/:username" element={<EmailActivation />} />
             <Route path="/ResetPassword" element={<ResetPassword />} />
             <Route path="/SuccessfullyResetPassword" element={<SuccessfullyResetPassword />} />
             <Route path="/AudioRecorderUploader" element={<AudioRecorderUploader />} />
-
-
             <Route path="*" element={<div className='container' style={{ backgroundColor: "#e6d1d0" }}><h1>this page Not Found</h1></div>} />
 
           </Routes>
