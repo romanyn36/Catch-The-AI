c596219d2b382191de62cbe3ce664957409793b5
Romani Nasrat Shawqi <romanyyy88@gmail.com>
2024-06-15 19:09:58 +0300

    contact us send email added

--- Backend/ai_media_detection/urls.py
+++ Backend/ai_media_detection/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('predict_media/', views.predict_media, name='predict_media'),
     path('get_detected_media/', views.get_detected_media, name='get_detected_media'),
     path('get_user_history/', views.get_user_history, name='get_user_history'),
+    path('send_contact_us_email/', views.send_contact_us_email, name='send_contact_us_email'),
+
 
     
 ]
--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -1,6 +1,6 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Subscription,Users,Admin,DataHistory
+from .models import Subscription,Users,Admin,DataHistory,ContactMessage
 
 
 # # # Register your models here.
@@ -8,6 +8,7 @@ admin.site.register(Users)
 admin.site.register(Admin)
 admin.site.register(Subscription)
 admin.site.register(DataHistory)
+admin.site.register(ContactMessage)
 
 
 # customizing the admin panel
