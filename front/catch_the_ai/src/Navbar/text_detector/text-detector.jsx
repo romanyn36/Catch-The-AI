4e53b0d3a1c3f32f98babdec25d07319e72a0596
Romani Nasrat Shawqi <romanyyy88@gmail.com>
2024-06-20 20:17:55 +0300

    connect image model and test som images

--- front/catch_the_ai/src/Navbar/text_detector/text-detector.jsx
+++ front/catch_the_ai/src/Navbar/text_detector/text-detector.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './text-detector.css';
 import { BASE_DOMAIN_URL } from '../../index';
 import AudioRecorderUploader from './AudioRecorderUploader';
-
+// loader component
+import { TailSpin } from 'react-loader-spinner'; 
 
 const mediaData = [
   { id: 1, name: "Image" },
@@ -13,10 +14,12 @@ const mediaData = [
 export class TextDetector extends Component {
   state = {
     selectedMediaType: "Image", // Set default media  "Image"
-    pulsatingMediaType: "Image", 
+    pulsatingMediaType: "Image",
     text: "",
     image: null,
-    result: ""
+    result: "",
+    isClicked: false,
+    file: null,
   };
 
   componentDidMount() {
@@ -28,18 +31,18 @@ export class TextDetector extends Component {
     this.setPulsatingMediaType(mediaType);
   };
 
-  handleAudioUpload = (e) => {
+  handleImageFile = (e) => {
     const file = e.target.files[0];
-    console.log("Selected audio file:", file);
+    this.setState({ file });
   };
+
+
 
   handleStartRecording = () => {
     console.log("Recording started...");
   };
 
-  handleAudioClick = () => {
-    document.getElementById('audio-upload').click();
-  };
+
 
   setPulsatingMediaType = (mediaType) => {
     this.setState({ pulsatingMediaType: mediaType });
@@ -51,90 +54,116 @@ export class TextDetector extends Component {
 
   predict_media = (mediaType) => {
     console.log(`Predicting media type for ${mediaType}...`);
+
     // fetch the data from the server using fetch
     if (mediaType === 'Image') {
       console.log('Image');
+      this.fetchDetectionSystem(mediaType);
     } else if (mediaType === 'Text') {
       console.log('Text');
       this.fetchDetectionSystem(mediaType);
     }
     else if (mediaType === 'Audio') {
       console.log('Audio');
+
     }
   }
 
   fetchDetectionSystem = (mediaType) => {
+    this.setState({result: ""})
+    this.setState({ isClicked: true });
+    console.log("image file ",  this.state.file)
     const formData = new FormData();
     formData.append('media_type', mediaType.toLowerCase());
     formData.append('text', this.state.text);
+    formData.append('media', this.state.file);
+
+    // get token from local storage if it exists
+    const token = localStorage.getItem('token');
     const text = this.state.text;
     // fetch the data from the server using fetch
     const url = `${BASE_DOMAIN_URL}/predict_media/`;
     fetch(url, {
       method: 'POST',
-      headers: {},
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
       body: formData
     })
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        this.setState({ result: data.result });
+        this.setState({ result: data.result, isClicked: false });
         console.log(this.state.result)
+
+
       })
       .catch((error) => {
         console.error('Error:', error);
+        this.setState({ isClicked: false });
       });
+
   }
 
   render() {
-    const { selectedMediaType, pulsatingMediaType, result } = this.state;
+    const { selectedMediaType, pulsatingMediaType, result, isClicked } = this.state;
 
     return (
       <div className="container ">
-      <div className="container  MainContainer1  ">
-        <h2 className="media-type-heading text-center">Select Media Type</h2>
-        <div className="media-type-buttons row justify-content-center">
-          {mediaData.map((media) => (
-            <div key={media.id} className="col-auto mb-2">
-              <button
-                className={`media-type-button ${pulsatingMediaType === media.name ? '' : ''}`}
-                onClick={() => this.handleFileSelect(media.name)}
-              >
-                {media.name}<br />
-              </button>
-            </div>
-          ))}
-        </div>
+        <div className="container  MainContainer1  ">
+          <h2 className="media-type-heading text-center">Select Media Type</h2>
+          <div className="media-type-buttons row justify-content-center">
+            {mediaData.map((media) => (
+              <div key={media.id} className="col-auto mb-2">
+                <button
+                  className={`media-type-button ${pulsatingMediaType === media.name ? '' : ''}`}
+                  onClick={() => this.handleFileSelect(media.name)}
+                >
+                  {media.name}<br />
+                </button>
+              </div>
+            ))}
+          </div>
 
-        <div className="row w-100 mt-4 p-2">
-          <div className="col-md-9 ">
-            {selectedMediaType === 'Image' && (
-              <div className="media-container">
-                <label htmlFor="file-upload" className="label">Drag and Drop</label>
-                <p className='txt'>Or</p>
-                <label htmlFor="file-upload" className="label">Upload Your Media</label>
-                <input id="file-upload" type="file" style={{ display: "none" }} />
-                <p className="txt1">Maximum size 10 Mb</p>
-              </div>
-            )}
-            {selectedMediaType === 'Text' && (
-              <div className="media-container">
-                <textarea className="text-area form-control media-container" style={{backgroundColor:"transparent"}} placeholder="Enter Your Text Here" value={this.state.text} onChange={this.handleTextChange}></textarea>
-              </div>
-            )}
-            {selectedMediaType === 'Audio' && (
-              <div className="media-container ">
-                <AudioRecorderUploader />
-              </div>
-            )}
-          </div>
-          <div className="col-md-3">
-            <h3 className="media-type-heading">Result:</h3>
-            <p className="media-type-heading">{result}</p>
-            <button className="btn btn-outline- submit-button mt-2 mb-2" onClick={() => this.predict_media(pulsatingMediaType)}>AI or Human?</button>
+          <div className="row w-100 mt-4 p-2">
+            <div className="col-md-9 ">
+              {selectedMediaType === 'Image' && (
+                <div className="media-container">
+                  <label htmlFor="file-upload" className="label">Drag and Drop</label>
+                  <p className='txt'>Or</p>
+                  <label htmlFor="file-upload" className="label">Upload Your Media</label>
+                  <input id="file-upload" type="file" style={{ display: "none" }} onChange={this.handleImageFile}/>
+                  <p className="txt1">Maximum size 10 Mb</p>
+                </div>
+              )}
+              {selectedMediaType === 'Text' && (
+                <div className="media-container">
+                  <textarea className="text-area form-control media-container" style={{ backgroundColor: "transparent" }} placeholder="Enter Your Text Here" value={this.state.text} onChange={this.handleTextChange}></textarea>
+                </div>
+              )}
+              {selectedMediaType === 'Audio' && (
+                <div className="media-container ">
+                  <AudioRecorderUploader />
+                </div>
+              )}
+            </div>
+            <div className="col-md-3">
+              <h3 className="media-type-heading">Result:</h3>
+              { isClicked &&
+              <div className="loader-container">
+                <TailSpin
+                  color="#00BFFF"
+                  height={50}
+                  width={50}
+                  timeout={3000} // Example timeout
+                />
+                <p className="text-dark">Loading...</p>
+              </div>}
+              <p className="media-type-heading">{result}</p>
+              <button className="btn btn-outline- submit-button mt-2 mb-2" disabled={isClicked} onClick={() => this.predict_media(pulsatingMediaType)}>AI or Human?</button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
