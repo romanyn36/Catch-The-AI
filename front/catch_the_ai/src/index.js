2b4b492106dd3367328fdac0ba05eb997a17f1a7
Romani Nasrat Shawqi <romanyyy88@gmail.com>
2024-07-12 03:28:15 +0300

    fix drowpdown menu

--- front/catch_the_ai/src/index.js
+++ front/catch_the_ai/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
-
+import 'bootstrap';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 // define base domain url 
 // export var BASE_DOMAIN_URL = 'http://127.0.0.1:8000';
 
